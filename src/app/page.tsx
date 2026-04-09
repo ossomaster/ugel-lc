ab236136483938223ab85955b8ddd9189feba23c
Stalin <j.stalin1227@gmail.com>
2026-04-09 15:57:26 -0500

    09.04.2026 -4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,12 +14,12 @@ import SeccionBuenasPracticas from "./components/secciones/SeccionBuenasPractica
 export default function Home() {
 	return (
 		<MainLayout>
-			{/* <InicioModal
-				src="https://scontent-lim1-1.xx.fbcdn.net/v/t39.30808-6/658162888_931327466285358_1504002055197489080_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=13d280&_nc_ohc=Xxnz5jfSzO0Q7kNvwEoWRl3&_nc_oc=AdpdF9cW6i1OCpuSAWcYQgKU__RH--lHUyFCMUMwLZonaEQ0S4sS-T0qkf433ezM6hc&_nc_zt=23&_nc_ht=scontent-lim1-1.xx&_nc_gid=pSqlrWdfW-iRQH2EQ16pzQ&_nc_ss=7a3a8&oh=00_Af3PLXfsu58Iss2KjK7aNn_b3A1q-GAlxvWxHM3pxHkVzA&oe=69D45353"
+			<InicioModal
+				src="https://scontent-lim1-1.xx.fbcdn.net/v/t39.30808-6/668711497_937004042384367_4572974055960939663_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=13d280&_nc_ohc=revmUgGLYU4Q7kNvwFrmQhX&_nc_oc=AdoRvqptMA1niA28fjdn-v0jNeBddFXEFHfsGGEb1JCnPHV-wfi8no92AgXbDvnpG-4&_nc_zt=23&_nc_ht=scontent-lim1-1.xx&_nc_gid=Yxvx8wl9UpU6dU2IlpaMLA&_nc_ss=7a3a8&oh=00_Af3hyL_K_rqc694EsTUtVub_ifHzy0IbDVjWxYWOrZhQgQ&oe=69DDDCC6"
 				alt=""
                 // Definir href para redirigir a otra dirección, sino dejar sin href para mostrar solo la imagen sin enlace
-				href=""
-			/> */}
+				href="https://www.ugellaconvencion.gob.pe/assets/files/2026/publicaciones/35.MM-N247-2026-GR-CUSCO-GEREDU-F-09042026.pdf"
+			/>
 			<MainHero />
 			<SeccionInformacionDestacada />
 			<SeccionNoticias />
