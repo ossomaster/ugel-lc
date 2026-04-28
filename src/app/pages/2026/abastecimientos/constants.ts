8dfa56d7b27e06d69f08f80814729db82859c5bb
Stalin <j.stalin1227@gmail.com>
2026-04-28 11:27:31 -0500

    28.04.2026 -1

--- src/app/pages/2026/abastecimientos/constants.ts
+++ src/app/pages/2026/abastecimientos/constants.ts
@@ -13,6 +13,17 @@ export type TDocumentoNormativo = {
 export const DOCUMENTOS_NORMATIVOS: TDocumentoNormativo[] = [
 	
 	{
+		numero: "190",
+		tipo: "Bien",
+		numcotizacion: "152",
+		descripcion: "SERVICIO DE ASISTENCIA TÉCNICA INFORMÁTICA PARA EL EQUIPO DE ESTADISTICA DE LA UGEL LA CONVENCIÓN",
+		archivoUrl: "/assets/files/2026/abastecimiento/190.COT.N152-F-28042026.pdf",
+		fechapublicacion: "28.04.2026",
+		inicio: "28.04.2026",
+		fin: "29.04.2026",
+	},
+
+	{
 		numero: "189",
 		tipo: "Bien",
 		numcotizacion: "151",
