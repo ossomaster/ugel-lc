af5d0ab047b1a8a0e560dacde6e439f4392b3f9f
Stalin <j.stalin1227@gmail.com>
2026-05-25 11:28:03 -0500

    25.05.2026 -3.2

--- src/app/pages/2026/cas/constants.ts
+++ src/app/pages/2026/cas/constants.ts
@@ -80,7 +80,7 @@ export const DOCUMENTOS_NORMATIVOS: TDocumentoNormativo[] = [
     final: "",
     feerratasfinal: "",
     comunicado1: {
-      url: "/assets/files/2026/contratacion-cas/05.COMUNICADO.N01-F-07052026.pdf",
+      url: "/assets/files/2026/contratacion-cas/07.COMUNICADO-F-25052026.pdf",
       fecha: "25/05/2026.",
     },
     comunicado2: "",
