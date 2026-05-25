17831594bbd32aba69be26bf6af67fbf2f795278
Stalin <j.stalin1227@gmail.com>
2026-05-25 14:45:54 -0500

    25.05.2026 -4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,11 +16,11 @@ export default function Home() {
         <MainLayout>
             {/* Usamos un Fragment para agrupar los componentes */}
             <>
-                {/* <InicioModal
-                    src="https://scontent-lim1-1.xx.fbcdn.net/v/t39.30808-6/703450710_970597702358334_4492101376319040712_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeGpH5WzF_PaqF4v-8HAxZ0TSGVwqeoCGVlIZXCp6gIZWWYFB9x0CYoeM99YUeHcgn5YN543uRld_xTUdMD2UZIK&_nc_ohc=eEh9_FtlNw4Q7kNvwGjNudZ&_nc_oc=AdoIwIAnu2Z51wvO--qt4kEFoVdxLRwvoCKK9Ng1KY2v_Puj-cIyiepWWhQJr-RTUpw&_nc_zt=23&_nc_ht=scontent-lim1-1.xx&_nc_gid=tvmurv8DeJhswXpAZLHqQQ&_nc_ss=7b2a8&oh=00_Af6Em5iFOsLm1Qzp3Fx8PQye7Tl8M3bEkk7WS1WyRjKaXQ&oe=6A151DA8"
+                <InicioModal
+                    src="https://scontent-lim1-1.xx.fbcdn.net/v/t39.30808-6/706317260_973813165370121_2690312653258245484_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeENQcgBhR6xsVysKW34r4c6cJq961hTLjlwmr3rWFMuOeXkCe3wUsic1VLRMXS9e_5nlYe3BXbKzdpPZ_kmRfqW&_nc_ohc=Yc2_Gzv-2EYQ7kNvwEtoPUl&_nc_oc=AdrQhRdGZ_URxS53bqxrWgBQ3H-R9OP3hN15Jua4V9Dob_9i34WETlSg6zJiOZoBj8M&_nc_zt=23&_nc_ht=scontent-lim1-1.xx&_nc_gid=BS0GtUXx2fJ_iYRqT13i2A&_nc_ss=7b2a8&oh=00_Af59OBj2JRlACcejWDF68iloaW6opOiBiGOAx0x7NX_lEg&oe=6A1A6BE7"
                     alt=""
                     href=""
-                /> */}
+                />
                 <MainHero />
             </>
             
