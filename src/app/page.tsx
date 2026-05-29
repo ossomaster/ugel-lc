a04f567bce7296775efa6882d314359881249087
Stalin <j.stalin1227@gmail.com>
2026-05-29 11:38:03 -0500

    29.05.2026 -1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,8 @@ export default function Home() {
             {/* Usamos un Fragment para agrupar los componentes */}
             <>
                 <InicioModal
-                    src="assets/files/2026/modal/simulacro-F-28052026.jpeg"
+                    src="https://scontent-lim1-1.xx.fbcdn.net/v/t39.30808-6/709092362_977130048371766_4428123261388950335_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=127cfc&_nc_ohc=kgFBdniq0oYQ7kNvwEmkh_4&_nc_oc=Adp1xxW1dFR6DpNpeyf6RDXL9-KgrWrsY_S2eD2TnouhiNB1A-vKR9rUBgcMLNFyeP8&_nc_zt=23&_nc_ht=scontent-lim1-1.xx&_nc_gid=z7-IB2rmxOsiMZDjyYewDg&_nc_ss=7b2a8&oh=00_Af4pm7UIIhtQ5Y5DQANOedw3nwxRwtvhiVpI-2JQGGCnmA&oe=6A1FADB7"
+                    //src="assets/files/2026/modal/simulacro-F-28052026.jpeg"
                     alt=""
                     href=""
                 />
